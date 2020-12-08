a32c0fb31d46a02111043a9ccd270438e97b26c5
Dan Moneypenny <catullus1973@gmail.com>
2020-12-08 00:43:20 -0800

    Add index controller and router.

---
+++ app/controllers/client/indexControllers.js
@@ -0,0 +1,16 @@
+const asyncHandler = require('../../middleware/handleAsync');
+
+/**
+ * @route   GET /
+ * @desc    view dashboard
+ * @access  private
+ */
+exports.dashboard = asyncHandler(async function(req, res, next) {
+  return res
+    .status(200)
+    .render('pages/index', {
+      success: true,
+      title: 'dashboard'
+    });
+
+});
